77aeceb69de6a3ef9fa77085c1abbd237208bc1a
MohammadRstm <mohammadrstmamal@gmail.com>
2025-07-20 09:49:21 +0300

    re-designed the folder system for checkout

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { HomePage } from './Pages/HomePage'
 import {Routes , Route} from 'react-router'
-import { ChecoutPage } from './Pages/CheckoutPage'
+import { ChecoutPage } from './Pages/checkout/CheckoutPage'
 import { OrdersPage } from './Pages/OrdersPage'
 import { TrackingPage } from './Pages/TrackingPage'
 
