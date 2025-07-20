8b251cb880734c1003fdd8d1a460b5c09f461dd3
MohammadRstm <mohammadrstmamal@gmail.com>
2025-07-20 08:43:41 +0300

    moved over the checkout page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { HomePage } from './Pages/HomePage'
 import {Routes , Route} from 'react-router'
+import { ChecoutPage } from './Pages/CheckoutPage'
+
 function App() {
   return (
     <>
     <Routes>
       <Route path="/" element={<HomePage/>}></Route>
-      <Route path="checkout" element={<div>Hello nigger </div>}></Route>
+      <Route path="checkout" element={<ChecoutPage/>}></Route>
     </Routes>
     </>
   )
